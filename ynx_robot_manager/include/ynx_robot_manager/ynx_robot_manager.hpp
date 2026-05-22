7e43fe0c7485c59b31e3b16177ebacc8708cc31c
Alexander Minor <alexander@minor.pw>
2026-05-22 18:25:12 -0400

    ynx_robot_manager added

---
+++ ynx_robot_manager/include/ynx_robot_manager/ynx_robot_manager.hpp
@@ -0,0 +1,64 @@
+#ifndef YNX_ROBOT_MANAGER__YNX_ROBOT_MANAGER_HPP_
+#define YNX_ROBOT_MANAGER__YNX_ROBOT_MANAGER_HPP_
+
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <moveit/planning_scene_interface/planning_scene_interface.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <cmath>
+
+#include "robot_manager_interfaces/action/joint_goal.hpp"
+#include "robot_manager_interfaces/action/pose_goal.hpp"
+#include "robot_manager_interfaces/srv/home.hpp"
+
+namespace ynx_robot_manager
+{
+  class YnxRobotManager : public rclcpp::Node {
+    public:
+      using JointGoal = robot_manager_interfaces::action::JointGoal;
+      using JointGoalHandle = rclcpp_action::ServerGoalHandle<JointGoal>;
+      using PoseGoal = robot_manager_interfaces::action::PoseGoal;
+      using PoseGoalHandle = rclcpp_action::ServerGoalHandle<PoseGoal>;
+      using Home = robot_manager_interfaces::srv::Home;
+
+      YnxRobotManager();
+      void setup();
+
+    private:
+      // Servers and Clients
+      rclcpp_action::Server<JointGoal>::SharedPtr joint_goal_action_server_;
+      rclcpp_action::Server<PoseGoal>::SharedPtr pose_goal_action_server_;
+      rclcpp::Service<Home>::SharedPtr home_service_;
+      rclcpp::CallbackGroup::SharedPtr service_cb_group_;
+
+      // Callbacks
+      rclcpp_action::GoalResponse joint_goal_handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const JointGoal::Goal> goal);
+      rclcpp_action::CancelResponse joint_goal_handle_cancel(const std::shared_ptr<JointGoalHandle> goal_handle);
+      void joint_goal_handle_accepted(const std::shared_ptr<JointGoalHandle> goal_handle);
+      void joint_goal_handle_execution(const std::shared_ptr<JointGoalHandle> goal_handle);
+      rclcpp_action::GoalResponse pose_goal_handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const PoseGoal::Goal> goal);
+      rclcpp_action::CancelResponse pose_goal_handle_cancel(const std::shared_ptr<PoseGoalHandle> goal_handle);
+      void pose_goal_handle_accepted(const std::shared_ptr<PoseGoalHandle> goal_handle);
+      void pose_goal_handle_execution(const std::shared_ptr<PoseGoalHandle> goal_handle);
+      void handle_home_service(const std::shared_ptr<Home::Request> request, std::shared_ptr<Home::Response> response);
+
+      // Helper Functions
+      double calculate_joint_distance(const std::vector<double>& current, const std::vector<double>& target);
+      double calculate_cartesian_distance(const geometry_msgs::msg::Point& p1, const geometry_msgs::msg::Point& p2);
+
+      // Variables
+      std::string ns_;
+      std::string tf_prefix_;
+      std::string planning_group_;
+      std::unique_ptr<moveit::planning_interface::MoveGroupInterface> move_group_;
+      std::unique_ptr<moveit::planning_interface::MoveGroupInterface::Plan> current_plan_;
+      std::unique_ptr<moveit::planning_interface::PlanningSceneInterface> planning_scene_interface_;
+      std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+      std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  };
+
+}  // namespace ynx_robot_manager
+
+#endif  // YNX_ROBOT_MANAGER__YNX_ROBOT_MANAGER_HPP_
---
+++ ynx_robot_manager/src/home.cpp
@@ -0,0 +1,35 @@
+#include "ynx_robot_manager/ynx_robot_manager.hpp"
+
+namespace ynx_robot_manager
+{
+  void YnxRobotManager::handle_home_service(
+      const std::shared_ptr<Home::Request> request,
+      std::shared_ptr<Home::Response> response) 
+  {
+    double velocity_scaling = std::max(0.01, std::min(abs(request->speed), 1.0));
+    double acceleration_scaling = velocity_scaling;
+    move_group_->clearPoseTargets();
+    move_group_->clearPathConstraints();
+    std::vector<double> home_positions = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; 
+    move_group_->setJointValueTarget(home_positions);
+    move_group_->setMaxVelocityScalingFactor(0.3);
+    move_group_->setMaxAccelerationScalingFactor(0.1);
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    bool success = (move_group_->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+    if (!success) {
+      RCLCPP_ERROR(this->get_logger(), "[Home Service] Planning failed. Cannot move home.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "[Home Service] Plan successful. Executing movement...");
+    auto exec_status = move_group_->execute(my_plan);
+    if (exec_status == moveit::core::MoveItErrorCode::SUCCESS) {
+        RCLCPP_INFO(this->get_logger(), "[Home Service] Successfully homed.");
+        response->success = true;
+        response->message = "Robot successfully homed.";
+    } else {
+        RCLCPP_ERROR(this->get_logger(), "[Home Service] Execution failed.");
+        response->success = false;
+        response->message = "Execution failed or was cancelled.";
+    }
+  }
+}  // namespace ynx_robot_manager
